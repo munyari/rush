617a5466d48ba0451ec47332b59b47d43a61e113
Panashe M. Fundira <fundirap@gmail.com>
2016-06-14 14:19:14 -0400

    add license

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
-use std::io::Write;
-use std::io;
-use std::io::stdout;
-use std::io::stdin;
+// Rush - A simple Unix shell written in Rust
+//     Copyright (C) 2016  Panashe M. Fundira
+
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use std::io::{stdin, stdout, Result, Write};
 use std::process::Command;
 
 fn main() {
