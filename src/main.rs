b192c6cb3fe009bde4c385d0a25c4c1fc12d8262
Panashe M. Fundira <fundirap@gmail.com>
2016-06-14 12:42:00 -0400

    improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::Write;
+use std::io;
 use std::io::stdout;
 use std::io::stdin;
 use std::process::Command;
@@ -14,17 +15,28 @@ fn main() {
     stdin().read_line(&mut user_input).expect("Failed to read line");
 
     for statement in user_input.split(";") {
-        let mut command_line = statement.split_whitespace();
-        let command = command_line.next().unwrap();
-        println!("command: {}", command);
-
-        let arguments = command_line.collect::<Vec<&str>>();
-        let mut out = Command::new(command)
-            .args(&arguments[..])
-            .spawn()
-            .expect("Invalid command");
-        let ecode = out.wait().expect("Ahh!!");
-        let exit_status = ecode.success();
-        println!("Exited {} ", exit_status);
+        if let Err(e) = run_statement(statement) {
+            println!("Invalid command: {}", e);
+        }
     }
+}
+
+fn run_statement(statement: &str) -> io::Result<()> {
+    let mut command_line = statement.split_whitespace();
+    let command = match command_line.next() {
+            Some(c) => c,
+            None    => return Ok(()),
+        };
+    println!("command: {}", command);
+
+    let arguments = command_line.collect::<Vec<&str>>();
+    let mut out = try!(Command::new(command)
+        .args(&arguments[..])
+        .spawn());
+
+    let ecode = try!(out.wait());
+    let exit_status = ecode.success();
+    println!("Exited {} ", exit_status);
+
+    Ok(())
 }
